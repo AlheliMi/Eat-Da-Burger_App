976da65a76c1fedd22b0f9738d853773631fbcd1
Alheli Miranda <alhealhe72@hotmail.com>
2021-01-15 23:33:52 -0600

    test02

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Turkey Burger", true);
+INSERT INTO burger (burger_name, devoured) VALUES ("Veggie Burger", true);
+INSERT INTO burger (burger_name) VALUES ("Big meat Burguer");
+INSERT INTO burger (burger_name) VALUES ("Delicius LBT Burger");
+INSERT INTO burger (burger_name, devoured) VALUES ("Two meat Burger", true);
+INSERT INTO burger (burger_name) VALUES ("CheesseBurger");
+
