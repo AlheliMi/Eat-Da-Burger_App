fcabee48b8d0098b0f43b9e45935d4e4053e1b50
Alheli Miranda <alhealhe72@hotmail.com>
2021-01-16 20:10:37 -0600

    deleting some things

--- Burger/db/seeds.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO burger (burger_name, devoured) VALUES ("Bacon Turkey Burger", true);
-INSERT INTO burger (burger_name, devoured) VALUES ("Veggie Burger", true);
-INSERT INTO burger (burger_name) VALUES ("Big meat Burguer");
-INSERT INTO burger (burger_name) VALUES ("Delicius LBT Burger");
-INSERT INTO burger (burger_name, devoured) VALUES ("Two meat Burger", true);
-INSERT INTO burger (burger_name) VALUES ("CheesseBurger");
-
