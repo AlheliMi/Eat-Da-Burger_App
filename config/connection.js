6ee2a50d39486ddc146804b563d070f7789540cd
Alheli Miranda <alhealhe72@hotmail.com>
2021-01-16 20:35:19 -0600

    fixing Heroku Bugs

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection =mysql.createConnection({
   host: "lfmerukkeiac5y5w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "ifn1y9lqyym0xxte",
   password: "ljr01nyi06fcr4r0",
   database: "q762olpblcctf6y8"
 });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
